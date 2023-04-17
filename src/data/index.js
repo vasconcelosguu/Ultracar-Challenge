b29ca6155792fba373ad91a5b49d1912b3dbce3b
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-17 15:30:44 -0300

    :bulb: Starting mock data

---
+++ src/data/index.js
@@ -0,0 +1,21 @@
+const jessicaId = '0938aa23-f153-4937-9f88-4858b24d6bce';
+const rafaelId = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
+const franciscoId = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
+const yagoId = '78460a91-f4da-4dea-a469-86fd2b8ccc84';
+
+module.exports = {
+  clients: [
+    {
+      name: 'Jessica',
+      lastName: 'Silva',
+      email: 'jessica@gmail.com',
+      password: 'jessicapassword',
+      id: jessicaId,
+      carModel: 'Peugeot',
+      part: {
+        name: 'Kit Suspensão',
+        price: 'R$ 1.399'
+      }
+    }
+  ]
+}
