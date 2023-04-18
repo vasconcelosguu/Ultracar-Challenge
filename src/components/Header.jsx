ff21f93885738a5540b719058f1bdeb51588c0f1
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-18 14:10:51 -0300

    :art: updating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,28 +16,28 @@ export default function Header() {
   };
 
   return(
-    <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
-		<div class="flex items-center flex-shrink-0 text-white mr-6">
-				<span class="text-2xl pl-2"><i class="em em-grinning"></i>{`Bem vindo(a), ${userInformations.name}`}</span>
+    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6 fixed w-full z-10 top-0">
+		<div className="flex items-center flex-shrink-0 text-white mr-6">
+				<span className="text-2xl pl-2"><i className="em em-grinning"></i>{`Bem vindo(a), ${userInformations.name}`}</span>
 		</div>
 
-		<div class="block lg:hidden">
-			<button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
-				<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+		<div className="block lg:hidden">
+			<button id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
+				<svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 					<title>Menu</title>
 					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
 				</svg>
 			</button>
 		</div>
 
-		<div class="w-full flex-grow lg:flex lg:items-center lg:w-auto pt-6 lg:pt-0 hidden" id="nav-content">
-			<ul class="list-reset lg:flex justify-end flex-1 items-center">
-				<li class="mr-3">
-					<Link class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:text-underline py-2 px-4" to='/homepage'>Home</Link>
-					<Link class="inline-block text-gray-200 no-underline hover:text-gray-400 hover:text-underline py-2 px-4" to='/services'>Serviços</Link>
+		<div className="w-full flex-grow lg:flex lg:items-center lg:w-auto pt-6 lg:pt-0 hidden" id="nav-content">
+			<ul className="list-reset lg:flex justify-end flex-1 items-center">
+				<li className="mr-3">
+					<Link className="inline-block text-gray-200 no-underline hover:text-gray-400 hover:text-underline py-2 px-4" to='/homepage'>Home</Link>
+					<Link className="inline-block text-gray-200 no-underline hover:text-gray-400 hover:text-underline py-2 px-4" to='/services'>Serviços</Link>
 				</li>
-				<li class="mr-3">
-					<button onClick={logoutFunc} class="inline-block text-gray-200 no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Logout<BiLogOut /></button>
+				<li className="mr-3">
+					<button onClick={logoutFunc} className="inline-block text-gray-200 no-underline hover:text-gray-200 hover:text-underline py-2 px-4">Logout<BiLogOut /></button>
 				</li>
 			</ul>
 		</div>
