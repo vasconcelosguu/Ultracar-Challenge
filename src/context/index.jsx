fefdc40759ad7977d854c26f9b4ea6902c3ccd9c
Gustavo Vasconcelos <vasconcelosgu604@gmail.com>
2023-04-18 14:11:31 -0300

    :fire: creating contexts states

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,11 +4,20 @@ export const MyContext = React.createContext();
 
 function Provider(props) {
   const [userInformations, setUserInformations] = useState('');
+  const [isInWork, setWorking] = useState(0);
+  const [initialDate, setInitialDate] = useState('')
+  const [initialHour, setInitialHour] = useState('')
 
   const contextValue = useMemo(() => ({
     userInformations,
-    setUserInformations
-  }), [userInformations]);
+    isInWork,
+    initialDate,
+    initialHour,
+    setUserInformations,
+    setWorking,
+    setInitialDate,
+    setInitialHour,
+  }), [userInformations, isInWork, initialDate, initialHour]);
 
   return (
     <MyContext.Provider value={contextValue}>
